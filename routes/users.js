ca90fbfec4d7e5d3a9241e129b88c83df86070e3
Zerdsexy <pr.thanawat_st@tni.ac.th>
2022-11-26 11:23:26 +0700

    WorkshopA_W3

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('MCU');
+  res.status(200).json({
+    full_name:'Thanawat Prapassuwan'
+   })
+});
+
+router.get('/bio', function(req, res, next) {
+  //res.send('MCU');
+  res.status(200).json({
+    full_name:'Thanawat Prapassuwan',
+    nickname:'NOCODEKING',
+    hobby:'NOCODING',
+    gitusername:"SEXYNOCODEKING"
+   })
 });
 
 module.exports = router;
